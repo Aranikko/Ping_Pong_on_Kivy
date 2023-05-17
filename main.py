0178080a3cd530af866dfa45c41be01a8b0f901b
Aranikko <gametv3020@gmail.com>
2023-05-17 19:18:45 +0300

    update

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from kivy.graphics.vertex_instructions import Rectangle, Ellipse
 from kivy.properties import Clock
 from kivy.metrics import dp
 from kivy.core.window import Window
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
 
 class MainWidget(RelativeLayout):
     
@@ -14,21 +16,20 @@ class MainWidget(RelativeLayout):
     size_ball = dp(50)
     
     # x vector speed
-    x_v_s = 5
+    x_v_s = 3
     # y vector speed
-    y_v_s = 5
+    y_v_s = 3
     
-    click = 0
-    click_2 = 0
+    colide_1 = False
+    colide_2 = False
     
-    width_window = 0
-    height_window = 0
+    lose = False
+    restart = StringProperty()
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.init_obj()
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
-        
         Clock.schedule_interval(self.update, 1/144)
 
     def _keyboard_closed(self):
@@ -45,28 +46,31 @@ class MainWidget(RelativeLayout):
             y1 += 50
         elif y1 > 0 and keycode[1] == 's':
             y1 -= 50
-            print(str(self.height) + ", " + str(y1 + s2))
         elif not self.height <= y2 + s2 and keycode[1] == 'up':
             y2 += 50
         elif y2 > 0 and keycode[1] == 'down':
             y2 -= 50
-            self.click_2 -= 1
+        elif self.lose and keycode[1] == 'r':
+            self.colide_1 = False
+            self.colide_2 = False
+    
+            self.lose = False
+            self.restart = " "
+            self.on_size()
         self.fst_player.pos = (x1, y1)
         self.sd_player.pos = (x2, y2)
         return True
     
     def init_obj(self):
         with self.canvas:
-            self.fst_player = Rectangle(size = (50, 100))
-            self.sd_player = Rectangle(size = (50, 100))
+            self.fst_player = Rectangle(size = (25, 150))
+            self.sd_player = Rectangle(size = (25, 150))
             self.ball = Ellipse(size=(self.size_ball, self.size_ball))
     
     def on_size(self, *args):
         self.fst_player.pos = (20, self.center_y-50)
         self.sd_player.pos = (self.width-70, self.center_y-50)
-        self.ball.pos = (self.center_x-25, self.center_y-25)
-        self.width_window = self.width
-        self.height_window = self.height 
+        self.ball.pos = (self.center_x-25, self.center_y-25) 
         
     def update_pos_ball(self):
         x, y = self.ball.pos
@@ -79,13 +83,15 @@ class MainWidget(RelativeLayout):
             y = self.height - self.size_ball
             self.y_v_s = -self.y_v_s
         
-        if x + self.size_ball > self.width:
-            x = self.width - self.size_ball
+        if self.colide_1:
+            x = self.width - self.size_ball - 75
             self.x_v_s = -self.x_v_s
+            self.colide_1 = False
         
-        if x < 0:
-            x = 0
+        if self.colide_2:
+            x = 0 + 75
             self.x_v_s = -self.x_v_s
+            self.colide_2 = False
             
         if y < 0:
             y = 0
@@ -109,10 +115,30 @@ class MainWidget(RelativeLayout):
         else:
             return False
     
+    def update_out_ball(self):
+        x, y = self.ball.pos
+        
+        if x - self.size_ball > self.width and not self.colide_1:
+            self.lose = True
+        
+        if x < 0 - self.size_ball and not self.colide_2:
+            self.lose = True
+    
+    def update_lose(self):
+        if self.lose:
+            self.restart = "Press <R>"
+    
     def update(self, dt):
         self.update_pos_ball()
+        
+        self.update_out_ball()
+        
+        self.update_lose()
+        if self.collishion(self.sd_player, self.ball):
+            self.colide_1 = True
+        
         if self.collishion(self.fst_player, self.ball):
-            print("collide")
+            self.colide_2 = True
         
 class Ping_Pong(App):
     pass
